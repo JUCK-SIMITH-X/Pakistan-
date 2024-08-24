e05939f07fe9f5c9f77656433fd4c908e479d9c7
JUCK-SIMITH-X <smhridoyariyan420@gmail.com>
2024-08-24 12:49:54 +0600

    Update antibotname.js

--- scripts/events/antibotname.js
+++ scripts/events/antibotname.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "antibotname",
   eventType: ["log:user-nickname"],
   version: "0.0.1",//beta
-  credits: "Nayan",
+  credits: "SM HRIDOY",
   description: "Prevent changing the Bot's nickname"
 };
 
