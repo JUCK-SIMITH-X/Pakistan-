5ad328d967a266253ec2ee6423f93b06dd201b7a
S.M Hridoy Ariyan <smhridoyariyan420@gmail.com>
2024-09-12 09:53:49 +0600

    Update and rename 404 Cyber Natural5 Video.js to 404 Hridoy Natural5 Video.js

--- scripts/commands/404 Hridoy Natural5 Video.js
+++ scripts/commands/404 Hridoy Natural5 Video.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "natural5 video",
   version: "1.0.0",
   permssion: 0,
-  credits: "Islamick Cyber Chat",
+  credits: "SM Hridoy",
   prefix:true,
   description: "NATURAL5  VEDIO",
   category: "I C C",
