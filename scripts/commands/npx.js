7b37598f6ce81a053f8a65add84f030496e1bb80
S.M Hridoy Ariyan <smhridoyariyan420@gmail.com>
2024-09-16 21:57:46 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf("🙉")==0 || body.indexOf("🙉")==0 || body.indexOf("😒")==0) {
+	if (body.indexOf("🫣")==0 || body.indexOf("🫣")==0 || body.indexOf("🫣")==0 || body.indexOf("😒")==0) {
 		var msg = {
 				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ 😚🥀",
 				attachment: fs.createReadStream(__dirname + `/cache/Mayabi.mp3`)
