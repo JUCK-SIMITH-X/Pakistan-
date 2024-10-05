72f681b9767de6fbd47425861f575a1189b030fc
S.M Hridoy Ariyan <smhridoyariyan420@gmail.com>
2024-10-05 12:05:54 +0600

    Update S.M Hridoy😌.js

--- scripts/commands/S.M Hridoy😌.js
+++ scripts/commands/S.M Hridoy😌.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf("😍")==0 || body.indexOf("😍")==0 || body.indexOf("😌")==0 || body.indexOf("😍")==0) {
+	if (body.indexOf("😽")==0 || body.indexOf("😽")==0 || body.indexOf("😌")==0 || body.indexOf("😽")==0) {
 		var msg = {
 				body: "কি্ঁগো্ঁ প্রে্ঁমে্ঁ হা্ঁবুডু্ঁবু্ঁ খা্ঁচ্ছ্ঁ না্ঁকি্ঁ তু্ঁমি্ঁ 😌🙀",
 				attachment: fs.createReadStream(__dirname + `/S.M.Hridoy/ki go preme.m4a`)
